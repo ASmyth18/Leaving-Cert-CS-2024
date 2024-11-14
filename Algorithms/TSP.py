8033c65c31d85e8ce0212c9071b8fe70317d2394
ASmyth18 <95776370+ASmyth18@users.noreply.github.com>
2024-11-14 09:45:15 +0000

    Update TSP.py

--- Algorithms/TSP.py
+++ Algorithms/TSP.py
@@ -54,3 +54,19 @@ distances = [
 
 min_cost = tsp_dynamic_programming(distances)
 print("Minimum cost:", min_cost)
+
+
+#Fibonacci sequence
+
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2  
+count = 1
+
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+print()
